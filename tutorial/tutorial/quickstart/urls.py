9269d6d5c95c8225ffd9bf08d50e389d9b8eba1d
Igor Sivak <igorsivak97@gmail.com>
2021-10-07 19:37:32 +0300

    add_cache

--- tutorial/tutorial/quickstart/urls.py
+++ tutorial/tutorial/quickstart/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
-
 from tutorial.tutorial.quickstart.views import *
+from django.views.decorators.cache import cache_page
 
 router = routers.DefaultRouter()
-router.register(r'person', PersonView)
+cache_page(80)(router.register(r'person', PersonView))
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -130,3 +130,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
